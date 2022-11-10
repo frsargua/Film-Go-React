928a165696b732e44790572553347c9f565bc12c
frsargua <frsargua@gmail.com>
2022-11-10 17:52:00 +0000

    updating vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://frsargua.github.io/",
+  base: "https://frsargua.github.io/frsargua.github.io.main",
   plugins: [react()],
 });
